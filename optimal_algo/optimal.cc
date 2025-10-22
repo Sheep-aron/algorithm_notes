38d257dad37a3b0af4386b7a7df060a937541ac1
Sheep-aron <kissing0927@gmail.com>
2025-10-23 06:13:42 +0800

    10.22练习

--- optimal_algo/optimal.cc
+++ optimal_algo/optimal.cc
@@ -51,3 +51,78 @@ public:
         }
     }
 };
+
+// 202. 快乐数
+class Solution 
+{
+public:
+    int bitSum(int n)
+    {
+        int sum = 0;
+        while (n)
+        {
+            sum += pow(n % 10, 2);
+            n /= 10;
+        }
+        return sum;
+    }
+
+    bool isHappy(int n) 
+    {
+        int slow = n, fast = bitSum(n);
+        while (slow != fast)
+        {
+            slow = bitSum(slow);
+            fast = bitSum(bitSum(fast));
+        }
+        return 1 == slow;
+    }
+};
+
+// 11. 盛最多水的容器
+class Solution 
+{
+public:
+    int maxArea(vector<int>& height) 
+    {
+        int n = height.size(), left = 0, right = n - 1, maxArea = 0;
+        while (left < right)
+        {
+            int area = min(height[left], height[right]) * (right - left);
+            maxArea = max(area, maxArea);
+            if (height[left] <= height[right])
+                ++left;
+            else
+                --right;
+        }
+        return maxArea;
+
+    }
+};
+
+// 611. 有效三角形的个数
+class Solution 
+{
+public:
+    int triangleNumber(vector<int>& nums) 
+    {
+        if (nums.size() < 3)
+            return 0;
+        int len = nums.size(), sum = 0;
+        sort(nums.begin(), nums.end());
+        for (int i = len - 1; i >= 2; --i)
+        {
+            for (int left = 0, right = i - 1; left < right;)
+            {
+                if (nums[left] + nums[right] <= nums[i])
+                    ++left;
+                else
+                {
+                    sum += right - left;
+                    --right;
+                }
+            }
+        }
+        return sum;
+    }
+};
